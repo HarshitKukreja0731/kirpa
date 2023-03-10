68827776d1468298b6a867133d03e771a16e54a2
Harshit <harshit387.be22@chitkara.edu.in>
2023-03-10 13:04:23 +0530

    changes done from o

--- arrayreverse.c
+++ arrayreverse.c
@@ -5,7 +5,11 @@ int main()
 
     scanf("%d",&n);
     int arr[n];
+    printf("this program reverses the array");
     for(i=n;i>0;i--){
+
+
+
         scanf("%d",&arr[i-1]);
     }
     for(j=0;j<n;j++){
