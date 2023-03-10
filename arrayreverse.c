c4055b12217ad260c6e2f23c3f10079bc6761cf6
HarshitKukreja0731 <122436300+HarshitKukreja0731@users.noreply.github.com>
2023-03-10 12:54:59 +0530

    Add files via upload

---
+++ arrayreverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+
+    scanf("%d",&n);
+    int arr[n];
+    for(i=n;i>0;i--){
+        scanf("%d",&arr[i-1]);
+    }
+    for(j=0;j<n;j++){
+        printf("%d ",arr[j]);
+    }
+    return 0;
+}
